2f64b29f3f97317ebd3355d1a6f835c7d01ee3ee
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-12-11 17:02:11 -0500

    working on miniParse

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -12,6 +16,24 @@ public class Main {
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
+    }
 
+    public ArrayList<String> miniParse(String item) throws Exception {
+        String regex = "[$&+,;=?@#|'<>.^*()%!-]";
+        Pattern pattern = Pattern.compile(regex);
+        String[] temp = pattern.split(item.toLowerCase());
+        ArrayList<String> itemList = new ArrayList<>();
+//        for(String items: temp){
+//            String patternString = "(item)";
+//            Pattern patternTemp = pattern.compile(item);
+//            Matcher matcher = patternTemp.matcher(items);
+//        }
+        // temp[0] = {name: milk}
+        Pattern newPattern = Pattern.compile(":(.+)");
+        for(String items: temp){
+            String[] newTemp = newPattern.split(items);
+            itemList.add(newTemp[0]);
+        }
+        return itemList;
     }
 }
